bc0b4fc5d25ccac461a1e2ecf8217e82996dcf6d
pfc15 <52254091+pfc15@users.noreply.github.com>
2022-12-11 22:29:16 -0300

    Add files via upload

--- ListaMusica/src/pacote/Main.java
+++ ListaMusica/src/pacote/Main.java
@@ -12,7 +12,8 @@ public class Main {
 		música.create("Heeeey jude", artista.getArmazem().getArtistas().get(0), 1955, "hey jude");
 		System.out.println(artista.getArmazem().getArtistas().get(0).getNome());
 		artista.read_música();
-		
+		Música jude = artista.armazem.busca_música("hey jude");
+		System.out.println(jude.getLetra());
 	}
 	
 }
